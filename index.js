3656149545d1534f740bc57d4d2077c3f90d1242
Aman <amanpathra22@gmail.com>
2023-12-21 19:41:15 +0530

    Initial Commit

---
+++ index.js
@@ -0,0 +1,24 @@
+const fs = require('fs');
+const path = require('path')
+
+const replaceThis = 'jay'
+const replaceWith = 'aman'
+
+const preview = false
+
+try {
+  const data = fs.readdir(path.join(__dirname, "/"), (err, data) => {
+
+    for (item of data) {
+      let newFile = item.replaceAll(replaceThis, replaceWith)
+      if (!preview) {
+        fs.rename(item, newFile, () => console.log('Rename Success'))
+      } else if (item != newFile) {
+        console.log(item + ' will be renamed to ' + newFile)
+      }
+    }
+
+  })
+} catch (err) {
+  console.error(err);
+}
